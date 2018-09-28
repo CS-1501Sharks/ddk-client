f2f0c6744d8bf0bb2801a6141fa2e1ff574b282d
Alisher <mukhametkali.a@gmail.com>
2018-09-28 15:39:03 +0600

    tt

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 // vue.config.js
 module.exports = {
-  lintOnSave: true,
-  dev: {
-    proxyTable: {
-      // proxy all requests starting with /api to jsonplaceholder
-      '/api': {
-        target: 'http://localhost:8081',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  }
+  lintOnSave: true//,
+  // dev: {
+  //   proxyTable: {
+  //     // proxy all requests starting with /api to jsonplaceholder
+  //     '/api': {
+  //       target: 'http://localhost:8081',
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         '^/api': ''
+  //       }
+  //     }
+  //   }
+  // }
 }
